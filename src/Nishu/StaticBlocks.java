fca2b5097ed58bff4f053efafd1f506363d611d7
Nisha Bhavsar <101039209+nisha-1712@users.noreply.github.com>
2022-03-21 15:59:15 +0530

    again Added block in program

--- src/Nishu/StaticBlocks.java
+++ src/Nishu/StaticBlocks.java
@@ -7,6 +7,9 @@ public class StaticBlocks {
     static {
         System.out.println("In Block concept .class file is created at the time of compilation ");
     }
+    static {
+        System.out.println("static block is executed before the instance block");
+    }
     public static void main(String[]args){
 
     }
