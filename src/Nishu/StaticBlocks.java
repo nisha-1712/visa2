ee45b8c02d04100cb3490533498663573d695b72
Nisha Bhavsar <101039209+nisha-1712@users.noreply.github.com>
2022-03-21 15:50:07 +0530

    Added another block in program

--- src/Nishu/StaticBlocks.java
+++ src/Nishu/StaticBlocks.java
@@ -4,6 +4,9 @@ public class StaticBlocks {
     static {
         System.out.println("Block concept");
     }
+    static {
+        System.out.println("In Block concept .class file is created at the time of compilation ");
+    }
     public static void main(String[]args){
 
     }
